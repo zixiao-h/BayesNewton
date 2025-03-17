1dc4e6bc6d2bba8e8a0deea42b82e004b974080d
Zixiao Hu <zixiao.hu.09@gmail.com>
2025-03-17 08:29:26 +0000

    added scipy to requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,14 @@ setup(
     version=__version__,
     packages=find_packages(),
     python_requires=">=3.6",
-    install_requires=["jax", "jaxlib", "objax", "tensorflow_probability", "numpy"],
+    install_requires=[
+        "jax",
+        "jaxlib",
+        "objax",
+        "tensorflow_probability",
+        "numpy",
+        "scipy",
+    ],
     url="https://github.com/AaltoML/BayesNewton",
     license="Apache-2.0",
 )
